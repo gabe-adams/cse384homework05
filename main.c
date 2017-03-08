00ce9e3127672f24adeaffa917f1cde9e7d2320a
gabe-adams <gabeadams5@gmail.com>
2017-03-08 13:30:03 -0500

    added getopts for cmdline option parsing. **for some reason doesn't hit default case (where it should print usage)**

--- main.c
+++ main.c
@@ -1,7 +1,51 @@
 #include <stdlib.h> //for exit succes
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdio.h>
 
+//called when no cmdline options or arguments are given
+void printUsage(char* name){
+	printf("USAGE: ./%s (-h)(-m)(-t)(-d arg) FILENAME\n", name);
+}
 
 int main(int argc, char* argv[]){
+
+	//initialize cmdline options
+	bool opt_h = false; bool opt_d = false; bool opt_m = false; bool opt_t = false;
+	char* opt_d_arg;
+	int opt = 0;
+	while((opt = getopt(argc, argv, "hmtd:")) != -1){
+		switch(opt){
+			case 'h': opt_h = true; 
+				break;
+			case 'm': opt_m = true; 
+				break;
+			case 't': opt_t = true; 
+				break;
+			case 'd': opt_d = true; 
+				opt_d_arg = optarg;
+				break;
+			default:
+			printUsage(argv[0]);
+			printf("hello\n");
+			return EXIT_FAILURE;
+		}
+	}
+
+	if(opt_h){
+		printf("-----------------------------------\n");
+		printf("Helpful information for Homework 05\n");
+		printf("-----------------------------------\n");
+		printf("This program is used to create a backup and track changes of an existing file.\n");
+		printf("-----------OPTIONS-----------------\n");
+		printf("h: display this information page and exit the program\n");
+		printf("m: disable meta-data duplication\n");
+		printf("t: append duplication time to file name\n");
+		printf("d: customize backup location. accepts an argument that is a path to backup folder\n");
+		return EXIT_SUCCESS;
+	}
+
+	//pass flags and optarg to business logic method
 
 	return EXIT_SUCCESS;
 }
